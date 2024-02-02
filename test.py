d3238bb549dd1a1bd0b359b4b789204173ff1347
Ido Ben Shaul <idobenshaul@Idos-MacBook-Pro.local>
2024-02-02 17:22:40 +0200

    reformatting

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ lang_model = SentenceTransformerEmbedder('thenlper/gte-small',
                             metric=cosine_similarity, threshold=0.85)
 df = SoftDataFrame(pd.read_csv("sample_data/Womens Fashion.csv"),
                    soft_columns=['name'], language_model=lang_model)
-
-filtered_df = df.soft_query("name ~ 'black dress'", threshold=0.85)
+#
+# df.soft_query("name ~= 'black dress'", threshold=0.85, inplace=True)
+filtered_df = df[df.similar_to('name', 'black dress', threshold=0.85)]
 import pdb; pdb.set_trace()
+# filtered_df = filtered_df_1.soft_query("name ~ 'dress'", threshold=0.85)
+
