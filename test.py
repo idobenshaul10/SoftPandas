49f00ca5150310ebcc737791b0fd983a71ae74ad
Ido Ben Shaul <idobenshaul@Idos-MacBook-Pro.local>
2024-02-03 18:59:35 +0200

    example

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 from core.data_types import InputDataType
 from core.soft_dataframe import SoftDataFrame
 from embedders.clip_embedder import OpenClipEmbedder
@@ -7,22 +6,23 @@ from embedders.sentence_transformer_embedder import SentenceTransformerEmbedder
 from sklearn.metrics.pairwise import cosine_similarity
 
 lang_model = SentenceTransformerEmbedder('thenlper/gte-small',
-                                metric=cosine_similarity, threshold=0.82, device="cpu")
+                                metric=cosine_similarity, threshold=0.85, device="cpu")
 
 
 vision_model = OpenClipEmbedder('ViT-B-32-256', metric=cosine_similarity,
-                                threshold=0.25, pretrained="datacomp_s34b_b86k")
+                                threshold=0.15, pretrained="datacomp_s34b_b86k")
 
 df = pd.read_csv("sample_data/men-swimwear.csv")
 df = SoftDataFrame(df, soft_columns={'NAME': InputDataType.text,
                                      'DESCRIPTION & COLOR': InputDataType.text},
-                                     # 'FABRIC': InputDataType.text},
                    models={InputDataType.text: lang_model, InputDataType.image: vision_model}
                    )
 
-relevant_price_items = df.query("PRICE < 600")
-df_filtered_desc = relevant_price_items.soft_query("'DESCRIPTION & COLOR' ~= 'red and black swim shorts'")
-df_filtered_desc = df_filtered_desc.add_soft_columns({'IMAGE': InputDataType.image}, inplace=False)
-df_filtered_image = df_filtered_desc.soft_query("'IMAGE' ~= 'red and black swim shorts'")
-print(df_filtered_image)
+df = df.add_soft_columns({'IMAGE': InputDataType.image}, inplace=False)
+df = df.soft_query("'DESCRIPTION & COLOR' ~= 'swim shorts'")
+df = df.soft_query("'IMAGE' ~= 'red and black'")
+df = df.query("PRICE < 600")
+
+print(df.head()['DESCRIPTION & COLOR'].values)
+
 
