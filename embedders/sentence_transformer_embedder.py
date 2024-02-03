364e097ba1727a39d7d089258b7532f38e317219
Ido Ben Shaul <idobenshaul@Idos-MacBook-Pro.local>
2024-02-03 15:12:40 +0200

    allowing to choose device for embedders

--- embedders/sentence_transformer_embedder.py
+++ embedders/sentence_transformer_embedder.py
@@ -1,11 +1,13 @@
 from sentence_transformers import SentenceTransformer
 from embedders.embedder import Embedder
 import numpy as np
+from typing import Callable
 
 
 class SentenceTransformerEmbedder(Embedder):
-    def __init__(self, model_name, metric, threshold):
-        super().__init__(model_name, metric, threshold)
+    def __init__(self, model_name: str, metric: Callable[[np.array, np.array], float],
+                 threshold: float, device: str = None):
+        super().__init__(model_name, metric, threshold, device)
         self.model = SentenceTransformer(self.model_name, device=self.device)
 
     def encode(self, data):
